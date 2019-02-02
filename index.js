9ba9758da8dff8123fd1cc0e79b80d7aef2bdef9
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-02-02 22:36:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -72,5 +72,11 @@ if (item != cart[item]){
 }
 
 function placeOrder(cardNumber) {
+  if (typeof(cardNumber) === "undefined"){
+    return "Sorry, we don't have a credit card on file for you."
+  } else{
+    cart = [];
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+  }
   // write your code here
 }
